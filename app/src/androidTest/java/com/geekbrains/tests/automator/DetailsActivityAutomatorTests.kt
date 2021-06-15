bbae2dd2a4b66069d7fef216e001b11cc8e684c6
пк <barcats@inbox.ru>
2021-06-15 21:06:19 +0300

    =

--- app/src/androidTest/java/com/geekbrains/tests/automator/DetailsActivityAutomatorTests.kt
+++ app/src/androidTest/java/com/geekbrains/tests/automator/DetailsActivityAutomatorTests.kt
@@ -3,10 +3,6 @@ package com.geekbrains.tests.automator
 import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
 import androidx.test.platform.app.InstrumentationRegistry
@@ -14,7 +10,6 @@ import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiObject2
 import androidx.test.uiautomator.Until
-import com.geekbrains.tests.R
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
